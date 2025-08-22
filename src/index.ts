4b9622719981f2e01ce44c22f7ac4fa5e3f7ab67
zat <fg42oh@gmail.com>
2025-08-22 11:14:41 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
 
 const port = process.env.PORT || 3000;
-const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
